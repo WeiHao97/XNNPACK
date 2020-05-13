a58e00ace3130beaee2623f22e6d2ea80e287811
WeiHao97 <37089196+WeiHao97@users.noreply.github.com>
2020-05-13 13:25:38 +0800

    Update mobilenet-v1.cc

--- models/mobilenet-v1.cc
+++ models/mobilenet-v1.cc
@@ -127,7 +127,7 @@ ExecutionPlan MobileNetV1(pthreadpool_t threadpool) {
   }else{
         std::cout<< "Can't open file"<< "\n";
   }
-  std::cout<< "Size: " << size << " Sparsity: "<< z_count/150528 <<"\n";
+  std::cout<< "Size: " << size << " Sparsity: "<< (float)z_count/150528 <<"\n";
   //std::generate(v2, v2 + 150528, std::ref(f32rng));
   std::generate(w0, w0 + 648, std::ref(f32rng));
   std::generate(w1, w1 + 24, std::ref(f32rng));
