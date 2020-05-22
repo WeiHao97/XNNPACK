ae74a535d58757df7567f2d3889a9881f33126d0
WeiHao97 <37089196+WeiHao97@users.noreply.github.com>
2020-05-22 17:51:12 +0800

    Update 16x1-minmax-neonfma-pipelined.c

--- src/f32-spmm/gen/16x1-minmax-neonfma-pipelined.c
+++ src/f32-spmm/gen/16x1-minmax-neonfma-pipelined.c
@@ -43,12 +43,12 @@ void xnn_f32_spmm_minmax_ukernel_16x1__neonfma_pipelined(
     size_t j = n;
 
 //wei sparsitycheck
-
+/*
     float32x4_t tmp1 = vaddq_f32(va0123,va4567);
     float32x4_t tmp2 = vaddq_f32(tmp1,va89AB);
     float32x4_t tmp3 = vaddq_f32(tmp2,vaCDEF);
     if(tmp3[0] != 0 || tmp3[1] != 0 || tmp3[2] != 0 || tmp3[3] != 0){
-    
+    */
 //wei sparsitycheck   
   
     do {
@@ -96,7 +96,7 @@ void xnn_f32_spmm_minmax_ukernel_16x1__neonfma_pipelined(
     i -= 16;
 
 //wei sparsitycheck
-
+/*
   }else{    
       do {
       uint32_t nnz = *nnzmap++;
@@ -122,7 +122,7 @@ void xnn_f32_spmm_minmax_ukernel_16x1__neonfma_pipelined(
     c += 16;
     a += 16;
     i -= 16;}
-
+*/
 //wei sparsitycheck
 
   }
